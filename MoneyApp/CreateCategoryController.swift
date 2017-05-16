cf0d4654df69a0bd8b6069bb58ca052ff42bbf51
Harjinder Gill <harjindergill@Harjinders-MBP.csusm.edu>
2017-05-16 14:04:08 -0700

    awdawd

--- MoneyApp/CreateCategoryController.swift
+++ MoneyApp/CreateCategoryController.swift
@@ -7,18 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
 
 class CreateCategoryController: UIViewController {
     
     
+    @IBOutlet weak var nameTextFeild: UITextField!
+    @IBOutlet weak var descriptionTexField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var _ = ad.saveContext()
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(CreateMoneyValueController.dismissKeyboard)))
     }
 
-    @IBOutlet weak var descriptionTextFeild: UITextField!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func dismissKeyboard() {
+        nameTextFeild.resignFirstResponder()
+        descriptionTexField.resignFirstResponder()
+    }
+    
+    func saveCat() {
+        let cat = Categories(context: context)
+        if nameTextFeild.text == nil {
+            nameTextFeild.text = ""
+        }
+        if descriptionTexField.text == nil {
+            descriptionTexField.text = ""
+        }
+        cat.name = nameTextFeild.text
+        cat.desc = descriptionTexField.text
+        let passes = ad.saveContext()
+        if !passes {
+            context.delete(cat)
+        }
+    }
+    @IBAction func makeCategory(_ sender: Any) {
+        saveCat()
+        let _ = navigationController?.popViewController(animated: true)
+    }
 }
